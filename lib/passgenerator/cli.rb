95230c30d061c5975ea3ff44cf40ecd8c43a0a5f
Alexandre Colas <colasalexandre@gmail.com>
2013-08-16 21:19:47 +0000

    Add version command to print version number.

--- lib/passgenerator/cli.rb
+++ lib/passgenerator/cli.rb
@@ -2,5 +2,11 @@ require "thor"
 
 module Passgenerator
   class CLI < Thor
+
+    desc "version", "print version number"
+    def version
+      puts "Passgenerator #{Passgenerator::VERSION}"
+    end
+
   end
 end
