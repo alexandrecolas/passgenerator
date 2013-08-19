00f0b6ed1051aa02a26ee7dcbc418e13fdc53451
Alexandre Colas <colasalexandre@gmail.com>
2013-08-19 21:25:59 +0000

    Create a generate command with --size option.

--- lib/passgenerator/cli.rb
+++ lib/passgenerator/cli.rb
@@ -8,5 +8,12 @@ module Passgenerator
       puts "Passgenerator #{Passgenerator::VERSION}"
     end
 
+    desc "generate", "generate a new password"
+    method_option :size, :type => :numeric, :aliases => "-s", :default => 8
+    def generate
+      range = [*'0'..'9', *'a'..'z', *'A'..'Z']
+      puts Array.new(options[:size]){range.sample}.join
+    end
+
   end
 end
